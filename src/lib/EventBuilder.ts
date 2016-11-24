596825ac3e45bca0a237df219908a3c211bc3150
Zack Campbell <zajrik@gmail.com>
2016-11-23 20:05:38 -0600

    Add beginnings of event building
    Still a ways to go. I have had no time lately. D:

---
+++ src/lib/EventBuilder.ts
@@ -0,0 +1,70 @@
+'use strict';
+import * as moment from 'moment';
+
+type int = number;
+type TimeArray = [string, int, int];
+
+export default class EventBuilder
+{
+	private _colorId: string;
+	private _summary: string;
+	private _description: string;
+	private _year: int;
+	private _month: int;
+	private _day: int;
+	private _start: string;
+	private _end: string;
+	private _length: number;
+
+	public constructor() {}
+	public color(color: int): this { this._colorId = String(color); return this; }
+	public summary(summary: string): this { this._summary = summary; return this; }
+	public description(desc: string): this { this._description = desc; return this; }
+
+	/**
+	 * Set the start time of the event. Accepts shorthand
+	 */
+	public start(time: string): this
+	{
+		this._start = this._parseTime(time)[0];
+		return this;
+	}
+
+	public end(time: string): this
+	{
+		this._end = this._parseTime(time)[0];
+		return this;
+	}
+
+	/**
+	 * Parse time shorthand (eg. 930a, 7p, 12:38a)
+	 */
+	private _parseTime(timeString: string): TimeArray
+	{
+		const regex: RegExp = /^(\d{1,2})(?::?(\d{2}))?(?:(a|p)m?)?$/;
+		const time: RegExpMatchArray = timeString.match(regex);
+		let hour: int = parseInt(time[1]);
+		let minutes: int = parseInt(time[2]) || 0;
+		let meridian: string = time[3] || null;
+
+		let hr: string;
+		let min: string;
+
+		if (!meridian && ([hour, minutes].join('')).length < 4)
+			throw new Error(`Provided time format is invalid: ${timeString}`);
+
+		if (minutes > 59)
+		{
+			minutes -= 60;
+			hour++;
+		}
+		if (hour <  12 && meridian === 'p') hour += 12;
+
+		if (hour <  10 && meridian === 'a') hr = `0${hour}`;
+		if (hour === 12 && meridian === 'a') hr = '00';
+		if (minutes === 0) min = '00';
+
+
+		return [`${hr}:${min}:00`, hour, minutes];
+	}
+}
---
+++ src/lib/structures/Event.ts
@@ -0,0 +1,32 @@
+'use strict';
+import * as moment from 'moment-timezone';
+
+export default class Event
+{
+	public colorId: string;
+	public summary: string;
+	public description: string;
+	public start: {
+		dateTime: string;
+		timeZone: string;
+	};
+	public end: {
+		dateTime: string;
+		timeZone: string;
+	};
+
+	public constructor(color: string, summary: string, description: string, start: string, end: string)
+	{
+		this.colorId = color;
+		this.summary = summary;
+		this.description = description;
+		this.start = {
+			dateTime: start,
+			timeZone: moment.tz.guess()
+		};
+		this.end = {
+			dateTime: end,
+			timeZone: moment.tz.guess()
+		};
+	}
+}
