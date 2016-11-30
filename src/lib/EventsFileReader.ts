5bdad9e788a081562ce09120959c49621c4853f1
Zack Campbell <zajrik@gmail.com>
2016-11-30 16:01:34 -0600

    Add generateEvents method

--- src/lib/EventsFileReader.ts
+++ src/lib/EventsFileReader.ts
@@ -2,6 +2,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import Constants from './util/Constants';
+import Event from './structures/Event';
+import EventBuilder from './EventBuilder';
 
 type int = number;
 
@@ -31,7 +33,6 @@ export default class EventsFileReader
 		{
 			throw new Error(Constants.errors.EVENTS_FILE_LOAD(err));
 		}
-		console.log(this._file);
 		this._eventRegex = /^(\d{1,2}) (\d{1,2}(?::?\d{2})?[ap]?m?) (\d{1,2}(?::?\d{2})?[ap]?m?)(?: ([^\|<]+))?(?: \| ([^\|<]+))?(?: << (\d{1,2}))?$/;
 		const colorRegex: RegExp = /color (\d{1,2})/;
 
@@ -46,5 +47,42 @@ export default class EventsFileReader
 		this._eventStrings.forEach(e => {
 			if (!this._eventRegex.test(e)) throw new Error(Constants.errors.EVENTS_FILE_EVENT_FORMAT(e));
 		});
+	}
+
+	/**
+	 * Generate events from the event strings within the events file.
+	 * The month will be overridden by any month override flags in
+	 * then event strings
+	 */
+	public generateEvents(opts: { year: int, month: int }): Event[]
+	{
+		let events: Event[] = [];
+		for (let event of this._eventStrings)
+		{
+			const data: RegExpMatchArray = event.match(this._eventRegex);
+			const day: int = parseInt(data[1]);
+			const start: string = data[2];
+			const end: string = data[3];
+			const description: string = data[4] || '';
+			const summaryOverride: string = data[5] || null;
+			const monthOverride: int = parseInt(data[6]) || null;
+
+			if (monthOverride && (monthOverride > 12 || monthOverride < 1))
+				throw new Error(Constants.errors.EVENTS_FILE_EVENT_INVALID_MONTH(event));
+
+			const builtEvent: Event = new EventBuilder()
+				.color(this._color)
+				.year(opts.year)
+				.month(monthOverride || opts.month)
+				.day(day)
+				.summary(summaryOverride || this._summary)
+				.description(description)
+				.start(start)
+				.end(end)
+				.prepare();
+
+			events.push(builtEvent);
+		}
+		return events;
 	}
 }
--- src/lib/util/Constants.ts
+++ src/lib/util/Constants.ts
@@ -4,6 +4,7 @@ type Errors = {
 	EVENTS_FILE_LOAD: (err: string) => string;
 	EVENTS_FILE_FORMAT: string;
 	EVENTS_FILE_EVENT_FORMAT: (event: string) => string;
+	EVENTS_FILE_EVENT_INVALID_MONTH: (event: string) => string;
 }
 
 type ValueConstants = {
@@ -15,6 +16,7 @@ const Constants: ValueConstants = <any> {}; // tslint:disable-line
 Constants.errors = {
 	EVENTS_FILE_LOAD: (err: string) => `There was an error loading the events file:\n${err}`,
 	EVENTS_FILE_FORMAT: 'The provided events file was formatted incorrectly',
-	EVENTS_FILE_EVENT_FORMAT: (event: string) => `Event is formatted incorrectly: ${event}`
+	EVENTS_FILE_EVENT_FORMAT: (event: string) => `Event is formatted incorrectly: ${event}`,
+	EVENTS_FILE_EVENT_INVALID_MONTH: (event: string) => `Event contains an invalid month override: ${event}`
 };
 export default Constants;
